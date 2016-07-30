94cf40bcbd05027f31c060aa836403930341fc2d
qiaoer <qiaoer2017@163.com>
2016-07-30 15:27:29 +0800

    导航栏完成

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function showSubNav(id){
+	var obj = document.getElementById(id);
+	obj.style.display="block";
+}
+function hideSubNav(id){
+	var obj = document.getElementById(id);
+	obj.style.display="none";
+}
+
